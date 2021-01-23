dc0abd9896708ede8997dc80d8125ca5aeca43f0
Jardel <jardel1101@gmail.com>
2021-01-23 15:46:33 -0300

    Clear AppointmentController

--- backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -7,23 +7,20 @@ import CreateAppointmentsService from '@modules/appointments/services/CreateAppo
 class AppointmentController {
 
   public async create(req: Request, res: Response): Promise<Response> {
-    console.log('CHEGOU AQUI')
-
     const user_id = req.user.id
     const { provider_id, datetime } = req.body
 
-    console.log(user_id, provider_id, datetime)
-
     const stringToDate = parseISO(datetime)
 
-    //console.log('parseada: '+stringToDate)
-
-    const services = container.resolve(CreateAppointmentsService)
+    const service = container.resolve(CreateAppointmentsService)
     
-    const Appointment = await services.execute({ provider_id, user_id, datetime: stringToDate })
-    //const Appointment = await services.create({ provider_id, user_id, datetime })
+    const appointment = await service.execute({
+      provider_id,
+      user_id,
+      datetime: stringToDate
+    })
 
-    return res.json(Appointment)
+    return res.json(appointment)
   }
 
 }
